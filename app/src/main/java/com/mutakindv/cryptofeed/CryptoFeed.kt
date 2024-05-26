d3197931fb700a81d60591cd6be7352810830a08
Mutakin <mutakin.email@gmail.com>
2024-05-26 21:37:32 +0700

    Create `CryptoFeed` model

---
+++ app/src/main/java/com/mutakindv/cryptofeed/CryptoFeed.kt
@@ -0,0 +1,23 @@
+package com.mutakindv.cryptofeed
+
+data class CryptoFeed(
+    val coinInfo: CoinInfo,
+    val raw: Raw
+)
+
+data class CoinInfo(
+    val id: String,
+    val name: String,
+    val fullName: String
+)
+
+data class Raw(
+    val usd: Usd
+)
+
+
+data class Usd(
+    val price: Double,
+    val changePctDay: Long
+)
+
