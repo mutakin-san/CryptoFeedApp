c0cd9211c87275624bba751e21fdd727006724b6
Mutakin <mutakin.email@gmail.com>
2024-06-01 19:43:27 +0700

    Create LoadCryptoFeedRemoteUseCaseTest

--- app/src/test/java/com/mutakindv/cryptofeed/LoadCryptoFeedRemoteUseCaseTest.kt
+++ app/src/test/java/com/mutakindv/cryptofeed/LoadCryptoFeedRemoteUseCaseTest.kt
@@ -1,10 +1,41 @@
 package com.mutakindv.cryptofeed
 
+import org.junit.Assert.assertEquals
 import org.junit.Test
 
 class LoadCryptoFeedRemoteUseCaseTest {
     @Test
-    fun testInit() {
+    fun testInitDoesNotLoad() {
+        val client = HttpClient.instance
+        LoadCryptoFeedRemoteUseCase()
 
+        assert(client.getCount == 0)
+    }
+
+    @Test
+    fun testLoadRequestData() {
+        val client = HttpClient.instance
+        val sut = LoadCryptoFeedRemoteUseCase()
+
+        sut.load()
+
+        assertEquals(1, client.getCount)
+
+    }
+}
+
+class LoadCryptoFeedRemoteUseCase {
+    fun load() {
+        HttpClient.instance.getCount = 1
+    }
+
+}
+
+class HttpClient private constructor(){
+
+    var getCount: Int = 0
+
+    companion object {
+        val instance = HttpClient()
     }
 }
