912478f61aa2fe82a071651299ded31c883c6e01
Mutakin <mutakin.email@gmail.com>
2024-06-01 20:21:33 +0700

    Refactoring `HttpClient` to be mutable

--- app/src/test/java/com/mutakindv/cryptofeed/LoadCryptoFeedRemoteUseCaseTest.kt
+++ app/src/test/java/com/mutakindv/cryptofeed/LoadCryptoFeedRemoteUseCaseTest.kt
@@ -6,7 +6,7 @@ import org.junit.Test
 class LoadCryptoFeedRemoteUseCaseTest {
     @Test
     fun testInitDoesNotLoad() {
-        val client = HttpClient.instance
+        val client = HttpClientSpy()
         LoadCryptoFeedRemoteUseCase()
 
         assert(client.getCount == 0)
@@ -14,7 +14,8 @@ class LoadCryptoFeedRemoteUseCaseTest {
 
     @Test
     fun testLoadRequestData() {
-        val client = HttpClient.instance
+        val client = HttpClientSpy()
+        HttpClient.instance = client
         val sut = LoadCryptoFeedRemoteUseCase()
 
         sut.load()
@@ -26,16 +27,28 @@ class LoadCryptoFeedRemoteUseCaseTest {
 
 class LoadCryptoFeedRemoteUseCase {
     fun load() {
-        HttpClient.instance.getCount = 1
+        HttpClient.instance.get()
     }
 
 }
 
-class HttpClient private constructor(){
+open class HttpClient{
+    companion object {
+        var instance = HttpClient()
+    }
 
+
+    open fun get() {
+
+    }
+}
+
+
+class HttpClientSpy : HttpClient() {
     var getCount: Int = 0
 
-    companion object {
-        val instance = HttpClient()
+    override fun get() {
+        getCount += 1
     }
+
 }
