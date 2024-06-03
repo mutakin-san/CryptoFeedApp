063ac619cf188545f9c0cabc239dc0ef6d86415a
Mutakin <mutakin.email@gmail.com>
2024-06-03 09:18:02 +0700

    Guarantee that we receive one error

--- app/src/main/java/com/mutakindv/cryptofeed/api/LoadCryptoFeedRemoteUseCase.kt
+++ app/src/main/java/com/mutakindv/cryptofeed/api/LoadCryptoFeedRemoteUseCase.kt
@@ -1,13 +1,28 @@
 package com.mutakindv.cryptofeed.api
 
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+
 
 interface HttpClient{
-    fun get()
+    fun get(): Flow<Exception>
 }
 
 class LoadCryptoFeedRemoteUseCase(private val client: HttpClient) {
-    fun load() {
-        client.get()
+    fun load() : Flow<Exception> = flow {
+        client.get().collect { error ->
+            if(error is Connectivity) {
+                emit(Connectivity())
+            }
+
+            if(error is BadResponse) {
+                emit(BadResponse())
+            }
+        }
     }
 }
+
+
+class Connectivity : Exception()
+class BadResponse : Exception()
 
--- app/src/test/java/com/mutakindv/cryptofeed/LoadCryptoFeedRemoteUseCaseTest.kt
+++ app/src/test/java/com/mutakindv/cryptofeed/LoadCryptoFeedRemoteUseCaseTest.kt
@@ -1,7 +1,12 @@
 package com.mutakindv.cryptofeed
 
+import com.mutakindv.cryptofeed.api.BadResponse
+import com.mutakindv.cryptofeed.api.Connectivity
 import com.mutakindv.cryptofeed.api.HttpClient
 import com.mutakindv.cryptofeed.api.LoadCryptoFeedRemoteUseCase
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.runBlocking
 import org.junit.Assert.assertEquals
 import org.junit.Test
 
@@ -33,6 +38,31 @@ class LoadCryptoFeedRemoteUseCaseTest {
         assertEquals(2, client.getCount)
 
     }
+    @Test
+    fun testLoadDeliverErrorOnClientSendConnectivityError() = runBlocking {
+        val (sut, client) = makeSut()
+
+        client.error = Connectivity()
+        var capturedError: Exception? = null
+        sut.load().collect{ error ->
+            capturedError = error
+        }
+
+
+        assertEquals(Connectivity::class.java, capturedError?.javaClass)
+    }
+    @Test
+    fun testLoadDeliverErrorOnClientSendBadResponseError() = runBlocking {
+        val (sut, client) = makeSut()
+
+        client.error = BadResponse()
+        var capturedError: Exception? = null
+        sut.load().collect{ error ->
+            capturedError = error
+        }
+
+        assertEquals(BadResponse::class.java, capturedError?.javaClass)
+    }
 
     private fun makeSut() : Pair<LoadCryptoFeedRemoteUseCase, HttpClientSpy> {
         val client = HttpClientSpy()
@@ -42,9 +72,10 @@ class LoadCryptoFeedRemoteUseCaseTest {
 
     private class HttpClientSpy : HttpClient {
         var getCount: Int = 0
-
-        override fun get() {
+        var error: Exception? = null
+        override fun get(): Flow<Exception> = flow {
             getCount += 1
+            error?.let { emit(it) }
         }
 
     }
