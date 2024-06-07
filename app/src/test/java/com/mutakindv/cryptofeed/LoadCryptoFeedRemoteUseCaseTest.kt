558390abe1e2e2db0ce46da032b548947922ed44
Mutakin <mutakin.email@gmail.com>
2024-06-07 19:28:23 +0700

    Move variable declaration to be inside the function

--- app/src/test/java/com/mutakindv/cryptofeed/LoadCryptoFeedRemoteUseCaseTest.kt
+++ app/src/test/java/com/mutakindv/cryptofeed/LoadCryptoFeedRemoteUseCaseTest.kt
@@ -124,51 +124,50 @@ class LoadCryptoFeedRemoteUseCaseTest {
     }
 
 
-    private val remoteCryptoFeedResponse = listOf(
-        RemoteCryptoFeedItem(
-            remoteCoinInfo = RemoteCoinInfo("1", "BTC", "Bitcoin", "imageUrl"),
-            remoteRaw = RemoteDisplay(
-                usd = RemoteUsd(
-                    price = 1.0,
-                    changePctDay = 1F
+    @Test
+    fun testLoadDeliverItemsOn200HttpResponseWithData() = runTest {
+        val remoteCryptoFeedResponse = listOf(
+            RemoteCryptoFeedItem(
+                remoteCoinInfo = RemoteCoinInfo("1", "BTC", "Bitcoin", "imageUrl"),
+                remoteRaw = RemoteDisplay(
+                    usd = RemoteUsd(
+                        price = 1.0,
+                        changePctDay = 1F
+                    )
                 )
-            )
-        ),
-        RemoteCryptoFeedItem(
-            remoteCoinInfo = RemoteCoinInfo("2", "BTC2", "Bitcoin 2", "imageUrl"),
-            remoteRaw = RemoteDisplay(
-                usd = RemoteUsd(
-                    price = 2.0,
-                    changePctDay = 2F
+            ),
+            RemoteCryptoFeedItem(
+                remoteCoinInfo = RemoteCoinInfo("2", "BTC2", "Bitcoin 2", "imageUrl"),
+                remoteRaw = RemoteDisplay(
+                    usd = RemoteUsd(
+                        price = 2.0,
+                        changePctDay = 2F
+                    )
                 )
-            )
-        ),
-    )
+            ),
+        )
 
 
-    private val cryptoFeedItems = listOf(
-        CryptoFeed(
-            coinInfo = CoinInfo("1", "BTC", "Bitcoin", "imageUrl"),
-            raw = Raw(
-                usd = Usd(
-                    price = 1.0,
-                    changePctDay = 1f
+        val cryptoFeedItems = listOf(
+            CryptoFeed(
+                coinInfo = CoinInfo("1", "BTC", "Bitcoin", "imageUrl"),
+                raw = Raw(
+                    usd = Usd(
+                        price = 1.0,
+                        changePctDay = 1f
+                    )
                 )
-            )
-        ),
-        CryptoFeed(
-            coinInfo = CoinInfo("2", "BTC2", "Bitcoin 2", "imageUrl"),
-            raw = Raw(
-                usd = Usd(
-                    price = 2.0,
-                    changePctDay = 2f
+            ),
+            CryptoFeed(
+                coinInfo = CoinInfo("2", "BTC2", "Bitcoin 2", "imageUrl"),
+                raw = Raw(
+                    usd = Usd(
+                        price = 2.0,
+                        changePctDay = 2f
+                    )
                 )
             )
         )
-    )
-
-    @Test
-    fun testLoadDeliverItemsOn200HttpResponseWithData() = runTest {
         expect(
             sut = sut,
             receivedHttpClient = HttpClientResult.Success(
