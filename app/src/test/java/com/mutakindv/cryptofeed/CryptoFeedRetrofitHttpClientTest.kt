57364f08ae0876e098a1bc086dd1217645462bfc
Mutakin <mutakin.email@gmail.com>
2024-06-12 08:30:17 +0700

    Delivers bad request error on 400 http response

--- app/src/test/java/com/mutakindv/cryptofeed/CryptoFeedRetrofitHttpClientTest.kt
+++ app/src/test/java/com/mutakindv/cryptofeed/CryptoFeedRetrofitHttpClientTest.kt
@@ -1,19 +1,25 @@
 package com.mutakindv.cryptofeed
 
 import app.cash.turbine.test
+import com.mutakindv.cryptofeed.api.BadRequestException
 import com.mutakindv.cryptofeed.api.ConnectivityException
 import com.mutakindv.cryptofeed.api.CryptoFeedRetrofitHttpClient
 import com.mutakindv.cryptofeed.api.CryptoFeedService
 import com.mutakindv.cryptofeed.api.HttpClientResult
+import com.mutakindv.cryptofeed.api.RemoteCryptoFeed
+import io.mockk.coEvery
 import io.mockk.coVerify
 import io.mockk.every
 import io.mockk.mockk
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.test.runTest
+import okhttp3.ResponseBody
 import org.junit.Assert.assertEquals
 import org.junit.Before
 import org.junit.Test
+import retrofit2.HttpException
+import retrofit2.Response
 import java.io.IOException
 
 
@@ -42,6 +48,25 @@ class CryptoFeedRetrofitHttpClientTest {
 
                 else -> {}
             }
+            awaitComplete()
+        }
+
+        coVerify(exactly = 1) {
+            service.get()
+        }
+    }
+
+
+    @Test
+    fun testGetFailsOn400HttpResponse() = runTest {
+        val response = Response.error<RemoteCryptoFeed>(400, ResponseBody.create(null, ""))
+        coEvery {
+            service.get()
+        } throws HttpException(response)
+
+        sut.get().test {
+            val receivedValue = awaitItem() as HttpClientResult.Failure
+            assertEquals(BadRequestException()::class.java, receivedValue.exception::class.java)
             awaitComplete()
         }
 
